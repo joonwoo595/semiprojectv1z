9444e4c98504b404480e63c7a80782dc864bfcf9
joonwoo595 <jonbeo312@gmail.com>
2025-03-07 14:19:08 +0900

    갤러리 구현 - 갤러리 글 작성 (미완성) 작성은 되는데 ggno를 int로 하면 오류발생 ggno가 null값인걸 보면 ggno값을 못가져오는 문제 같음

---
+++ src/main/java/com/example/zzyzzy/semiprojectv1/domain/NewGalleryDTO.java
@@ -0,0 +1,17 @@
+package com.example.zzyzzy.semiprojectv1.domain;
+
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class NewGalleryDTO {
+
+    private int ggno;
+    private String title;
+    private String userid;
+    private String simgname;
+    private String contents;
+}
